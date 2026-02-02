43c74147b2f0ba48c709319bbc2840131b09af51
Tomer <tomer10.katz@gmail.com>
2026-02-02 18:46:08 +0200

    no warning coused by updating to 2026

--- src/main/java/frc/demacia/utils/motors/TalonFXMotor.java
+++ src/main/java/frc/demacia/utils/motors/TalonFXMotor.java
@@ -26,6 +26,7 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.demacia.utils.Data;
 import frc.demacia.utils.log.LogManager;
@@ -433,7 +434,7 @@ public class TalonFXMotor extends TalonFX implements MotorInterface {
           value -> {
             if (value) {
               if (!configPidFf.isScheduled()) {
-                configPidFf.schedule();
+                CommandScheduler.getInstance().schedule(configPidFf);
               }
             } else {
               if (configPidFf.isScheduled()) {
@@ -473,7 +474,7 @@ public class TalonFXMotor extends TalonFX implements MotorInterface {
         value -> {
           if (value) {
             if (!configMotionMagic.isScheduled()) {
-              configMotionMagic.schedule();
+              CommandScheduler.getInstance().schedule(configMotionMagic);
             }
           } else {
             if (configMotionMagic.isScheduled()) {
