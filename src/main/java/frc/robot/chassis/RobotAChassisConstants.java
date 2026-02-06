903d7a3e3b152bea5fee8171b01cc4097f888e9b
Tomer <tomer10.katz@gmail.com>
2026-02-06 14:08:56 +0200

    add doc to the robot a chassis constants

--- src/main/java/frc/robot/chassis/RobotAChassisConstants.java
+++ src/main/java/frc/robot/chassis/RobotAChassisConstants.java
@@ -10,18 +10,32 @@ import frc.demacia.utils.sensors.CancoderConfig;
 import frc.demacia.utils.sensors.PigeonConfig;
 import frc.demacia.vision.TagPose;
 
+/** 
+ * Robot A Chasis
+ * the chassis that currenty contains the intake and the shinua
+ */
 public final class RobotAChassisConstants {
+
+    /** The name of the Chasssi */
     public static final String NAME = "Robot A Chassis";
+    /** The canbus that is used in the chassis, the chassis only contains the rio canbus */
     public static final Canbus CANBUS = Canbus.Rio;
 
+    /** Drive Motor Constant */
     private static final class DriveMotor {
+        /** The name of the motor */
         private static final String NAME = "Drive Motor";
 
+        /** The gear ratio of the drive motor, currenty R2 */
         private static final double GEAR_RATIO = Mk5nConstants.R2.driveGearRatio;
+        /** The wheel diamter of the moduel in meter */
         private static final double WHEEL_DIAMETER = Mk5nConstants.WHEEL_DIAMETER;
+        /** Set if the motor is brake or coast */
         private static final boolean IS_BRAKE = true;
+        /** Set if the motor is clockwise or inverted */
         private static final boolean IS_INVERT = true;
 
+        /* Drive Motor PID and FF */
         private static final double KP = 0d;
         private static final double KI = 0d;
         private static final double KD = 0d;
@@ -30,6 +44,12 @@ public final class RobotAChassisConstants {
         private static final double KA = 0d;
         private static final double KG = 0d;
 
+        /**
+         * Function to get the drive motor config of a module
+         * @param id the id of the motor, from 1 to 62
+         * @param moduleName the name of the module (eg. "Front Left Module", "Front Right Module")
+         * @return the config of the drive motor with all the constants
+         */
         private static TalonFXConfig getDriveMotor(int id, String moduleName) {
             return new TalonFXConfig(id, CANBUS, RobotAChassisConstants.NAME + "/" + moduleName + "/" + NAME)
                     .withBrake(IS_BRAKE)
@@ -39,14 +59,21 @@ public final class RobotAChassisConstants {
         }
     }
 
+    /** Steer Motor Constants */
     private static final class SteerMotor {
+        /** The name of the motor */
         private static final String NAME = "Steer Motor";
 
+        /** The amount of time it will take the motor to go from 0 to 12 volt, in seconds */
         private static final double RAMP_TIME = 0.25;
+        /** The gear ratio of the steer in Mk5n */
         private static final double GEAR_RATIO = Mk5nConstants.STEER_GEAR_RATIO;
+        /**  Set if the motor is at brake or coast*/
         private static final boolean IS_BRAKE = true;
+        /** Set if the motor is clockwise or inverted */
         private static final boolean IS_INVERT = true;
 
+        /* Steer Motor PID + FF*/
         private static final double KP = 0d;
         private static final double KI = 0d;
         private static final double KD = 0d;
@@ -55,6 +82,12 @@ public final class RobotAChassisConstants {
         private static final double KA = 0d;
         private static final double KG = 0d;
 
+        /**
+         * Function to get a steer motor of a module
+         * @param id the id of the motor, from 1 to 62
+         * @param moduleName the name of the module (eg. "Front Left Module", "Front Right Module")
+         * @return the config of the steer motor with all the constants
+         */
         private static TalonFXConfig getSteerMotor(int id, String moduleName) {
             return new TalonFXConfig(id, CANBUS, RobotAChassisConstants.NAME + "/" + moduleName + "/" + NAME)
                     .withBrake(IS_BRAKE)
@@ -65,12 +98,21 @@ public final class RobotAChassisConstants {
         }
     }
 
+    /** The Pigeon id */
     private static final int PIGEON_ID = 14;
-    private static final PigeonConfig pigeonConfig = new PigeonConfig(PIGEON_ID, CANBUS, NAME + "/" + "Pigeon");
+    /** The  config of the pigeon*/
+    private static final PigeonConfig PIGEON_CONFIG = new PigeonConfig(PIGEON_ID, CANBUS, NAME + "/" + "Pigeon");
 
+    /**
+     * Function to get the all the swerve module configs
+     * @param offsets the offset of all the modules based on the module order
+     * @see Module_Order is Front Left, Front Right, Back Left, Back Right
+     * @return all the swerve module configs in the module order
+     */
     private static SwerveModuleConfig[] getSwerveModuleConfigs(double[] offsets) {
         SwerveModuleConfig[] ans = new SwerveModuleConfig[4];
         for (int i = 0; i < 4; i++) {
+            /* set the name of the module */
             String moduleName = "Error";
             switch (i) {
                 case 0:
@@ -93,6 +135,7 @@ public final class RobotAChassisConstants {
                     break;
             }
 
+            /* set a new config based on the steer motor and drvie motor constatns classes */
             ans[i] = new SwerveModuleConfig(
                     moduleName,
                     SteerMotor.getSteerMotor(i * 3 + 2, moduleName),
@@ -107,7 +150,8 @@ public final class RobotAChassisConstants {
         return ans;
     }
 
-    private static final SwerveModuleConfig[] modules = getSwerveModuleConfigs(
+    /** The swerve modules config using the function */
+    private static final SwerveModuleConfig[] MODULES = getSwerveModuleConfigs(
             new double[] {
                     /* Front Left Offset: */ 0d,
                     /* Front Right Offset: */ 0d,
@@ -115,5 +159,6 @@ public final class RobotAChassisConstants {
                     /* Back Right Offset: */ 0
             });
 
-    public static final ChassisConfig CHASSIS_CONFIG = new ChassisConfig(NAME, modules, pigeonConfig, new TagPose[] {});
+    /** The Chassis config with the chassis name, modules config, pigeon config and tags */
+    public static final ChassisConfig CHASSIS_CONFIG = new ChassisConfig(NAME, MODULES, PIGEON_CONFIG, new TagPose[] {});
 }
