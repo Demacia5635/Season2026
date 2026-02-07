1b3c7e27a4cccfc170208102ec554b65c1c00625
haghanuka <idan12343567@gmail.com>
2026-02-07 12:29:03 +0200

    need to test on robot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,16 +4,16 @@
 
 package frc.robot;
 
+import static frc.robot.Constants.*;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.demacia.utils.controller.CommandController;
-import frc.demacia.utils.controller.CommandController.ControllerType;
-import frc.demacia.utils.log.LogManager;
-import frc.robot.climb.commands.ControllerClimb;
+import frc.robot.climb.commands.StateBasedClimb;
 import frc.robot.climb.subsystems.Climb;
 
 /**
@@ -27,11 +27,11 @@ import frc.robot.climb.subsystems.Climb;
  */
 public class RobotContainer implements Sendable {
   Climb climb;
-  CommandController controller;
-  ControllerClimb command;
-  public static boolean isComp = false;
-  private static boolean hasRemovedFromLog = false;
-  public static boolean isRed = false;
+  StateBasedClimb stateBasedClimb;
+
+  // public static Chassis chassis;
+  // CommandController driverController = new CommandController(0,
+  // ControllerType.kPS5);
 
   // The robot's subsystems and commands are defined here...
 
@@ -42,11 +42,28 @@ public class RobotContainer implements Sendable {
    */
   public RobotContainer() {
     SmartDashboard.putData("RC", this);
+    // chassis = new Chassis(MK4iChassisConstants.CHASSIS_CONFIG);
     climb = new Climb();
-    controller = new CommandController(1, ControllerType.kXbox);
-    command = new ControllerClimb(controller, climb);
+
     // Configure the trigger bindings
+    addStatesToElasticForTesting();
     configureBindings();
+  }
+
+  public void addStatesToElasticForTesting() {
+    SendableChooser<RobotCommon.robotStates> robotStateChooser = new SendableChooser<>();
+    for (RobotCommon.robotStates state : RobotCommon.robotStates.class.getEnumConstants()) {
+      robotStateChooser.addOption(state.name(), state);
+    }
+    robotStateChooser.onChange(state -> RobotCommon.currentState = state);
+    SmartDashboard.putData("Robot State Chooser", robotStateChooser);
+
+    SendableChooser<RobotCommon.Shifts> shiftsChooser = new SendableChooser<>();
+    for (RobotCommon.Shifts state : RobotCommon.Shifts.class.getEnumConstants()) {
+      shiftsChooser.addOption(state.name(), state);
+    }
+    shiftsChooser.onChange(state -> RobotCommon.currentShift = state);
+    SmartDashboard.putData("Shifts Chooser", shiftsChooser);
   }
 
   /**
@@ -63,34 +80,37 @@ public class RobotContainer implements Sendable {
    * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
+  public static boolean isShooting = false;
+
   private void configureBindings() {
-    climb.setDefaultCommand(command);
-  }
-
-  public static boolean getIsRed() {
-    return isRed;
-  }
-
-  public static void setIsRed(boolean isRed) {
-    RobotContainer.isRed = isRed;
-  }
-
-  public static boolean getIsComp() {
-    return isComp;
-  }
-
-  public static void setIsComp(boolean isComp) {
-    RobotContainer.isComp = isComp;
-    if (!hasRemovedFromLog && isComp) {
-      hasRemovedFromLog = true;
-      LogManager.removeInComp();
-    }
+    // DriveCommand driveCommand = new DriveCommand(chassis, driverController);
+    // chassis.setDefaultCommand(driveCommand);
   }
 
   @Override
   public void initSendable(SendableBuilder builder) {
-    builder.addBooleanProperty("isRed", RobotContainer::getIsRed, RobotContainer::setIsRed);
-    builder.addBooleanProperty("isComp", RobotContainer::getIsComp, RobotContainer::setIsComp);
+    builder.addBooleanProperty("is comp", () -> RobotCommon.isComp, (isComp) -> RobotCommon.isComp = isComp);
+    builder.addBooleanProperty("is red", () -> RobotCommon.isRed, (isRed) -> RobotCommon.isRed = isRed);
+
+    builder.addBooleanProperty("change is Robot Calibrated for testing", () -> RobotCommon.isRobotCalibrated,
+        (isRobotCalibrated) -> RobotCommon.isRobotCalibrated = isRobotCalibrated);
+    builder.addDoubleProperty("change Accuracy for testing", () -> RobotCommon.targetAccuracy,
+        (targetAccuracy) -> RobotCommon.targetAccuracy = targetAccuracy);
+  }
+
+  static public void updateCommon() {
+    Translation2d currentPoseFromHub = RobotCommon.currentRobotPose.getTranslation().minus(HUB_POS);
+    RobotCommon.currentDistanceFromTarget = currentPoseFromHub.getNorm();
+    RobotCommon.currentAngleFormTarget = currentPoseFromHub.getAngle().getRadians();
+    RobotCommon.currentWantedTurretAngle = RobotCommon.currentWantedTurretAngle
+        - RobotCommon.currentRobotPose.getRotation().getRadians();
+
+    Translation2d futurePoseFromHub = RobotCommon.futureRobotPose.getTranslation().minus(HUB_POS);
+    RobotCommon.futureDistanceFromTarget = futurePoseFromHub.getNorm();
+    RobotCommon.futureAngleFormTarget = futurePoseFromHub.getAngle().getRadians();
+    RobotCommon.futureWantedTurretAngle = RobotCommon.futureWantedTurretAngle
+        - RobotCommon.futureRobotPose.getRotation().getRadians();
+
   }
 
   /**
--- src/main/java/frc/robot/climb/constants/ClimbConstants.java
+++ src/main/java/frc/robot/climb/constants/ClimbConstants.java
@@ -1,70 +1,77 @@
 package frc.robot.climb.constants;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import frc.demacia.utils.motors.BaseMotorConfig.Canbus;
 import frc.demacia.utils.sensors.DigitalEncoderConfig;
 import frc.demacia.utils.motors.TalonFXConfig;
 import frc.demacia.utils.motors.TalonSRXConfig;
 
 public class ClimbConstants {
-    public static final int MOTOR_ID_ARMS = 51;
-    public static final Canbus CANBUS = Canbus.Rio;
-    public static final boolean WITH_BRAKE_ARMS = true;
-    public static final double MAX_CURRENT = 40;
-    public static final double DIAMETER_ARMS = 0;
-    public static final double GEAR_RATIO_ARMS = 1 / 24;
-    public static final boolean WITH_INVERT_ARMS = false;
-    public static final double POWER_TO_LOWER_ARMS = 0.2;
-    public static final double POWER_TO_RAISE_ARMS = 0.2;
-    public static final double TIME_TO_RAISE_ARMS = 0.2;
-    public static final double TIME_TO_RAISE_ARMS_AFTER_CLIMB = 0;
-    public static final double ANGLE_ARMS_RAISED = 0.6399424235362409; // radians
-    public static final double ANGLE_ARMS_LOWERED = Math.toRadians(0);
-    public static final double ARMS_ANGLE_CLOSED = 3.901700996125844; // radians
-    public static final double armsOffset = 0;
-    public static final double DISTANCE_TOLERANCE_TO_CLOSE_ARMS = 0.05; // meters
+        public static final int MOTOR_ID_ARMS = 51;
+        public static final Canbus CANBUS = Canbus.Rio;
+        public static final boolean WITH_BRAKE_ARMS = true;
+        public static final double MAX_CURRENT = 40;
+        public static final double DIAMETER_ARMS = 0;
+        public static final double GEAR_RATIO_ARMS = 1 / 24;
+        public static final boolean WITH_INVERT_ARMS = false;
+        public static final double POWER_TO_LOWER_ARMS = 0.2;
+        public static final double POWER_TO_RAISE_ARMS = 0.2;
+        public static final double TIME_TO_RAISE_ARMS = 0.2;
+        public static final double TIME_TO_RAISE_ARMS_AFTER_CLIMB = 0;
+        public static final double ANGLE_ARMS_RAISED = 0.6399424235362409; // radians
+        public static final double ANGLE_ARMS_LOWERED = Math.toRadians(0);
+        public static final double ARMS_ANGLE_CLOSED = 3.901700996125844; // radians
+        public static final double armsOffset = 0;
+        public static final double CHASSIS_TOLERANCE = 0.05; // meters
+        public static final double rotationKp = 2.2;
+        public static final double driveKp = 1.3;
+        public static Pose2d targetRightSide = Pose2d.kZero;
+        public static Pose2d targetLeftSide = Pose2d.kZero;
+        public static Pose2d targetToFullyCloseArms = Pose2d.kZero;
+        public static Pose2d targetToOpenArmsAfterClimb = Pose2d.kZero;
 
-    public static final int MOTOR_ID_LEVER = 52;
-    public static final boolean WITH_BRAKE_LEVER = false;
-    public static final double DIAMETER_LEVER = 0;
-    public static final double LEVER_GEAR_RATIO = 1 / 64;
-    public static final boolean WITH_INVERT_LEVER = false;
-    public static final double POWER_TO_OPEN_LEVER = 0.2;
-    public static final double POWER_TO_CLOSE_LEVER = -0.2;
-    public static final double ANGLE_LEVER_CLOSE = -0.4393564709457465; // radians
-    public static final double ANGLE_LEVER_OPEN = 0.2854944237345549; // radians
-    public static final double CLOSE_LEVER_TOLERANCE = Math.toRadians(5); // radians
-    public static final double POWER_TO_KEEP_HEIGHT = 0.05;
+        public static final int MOTOR_ID_LEVER = 52;
+        public static final boolean WITH_BRAKE_LEVER = false;
+        public static final double DIAMETER_LEVER = 0;
+        public static final double LEVER_GEAR_RATIO = 1 / 64;
+        public static final boolean WITH_INVERT_LEVER = false;
+        public static final double POWER_TO_OPEN_LEVER = 0.2;
+        public static final double POWER_TO_CLOSE_LEVER = -0.2;
+        public static final double ANGLE_LEVER_CLOSE = -0.4393564709457465; // radians
+        public static final double ANGLE_LEVER_OPEN = 0.2854944237345549; // radians
+        public static final double CLOSE_LEVER_TOLERANCE = Math.toRadians(5); // radians
+        public static final double POWER_TO_KEEP_HEIGHT = 0.05;
 
-    public static final double KS = 0;
-    public static final double KV = 0;
-    public static final double KA = 0;
-    public static final double KP = 0;
-    public static final double KG = 0;
-    public static final double KD = 0;
-    public static final double KI = 0;
+        public static final double KS = 0;
+        public static final double KV = 0;
+        public static final double KA = 0;
+        public static final double KP = 0;
+        public static final double KG = 0;
+        public static final double KD = 0;
+        public static final double KI = 0;
 
-    public static final TalonSRXConfig ARMS_MOTOR_CONFIG = new TalonSRXConfig(MOTOR_ID_ARMS, "arms motor")
-            .withBrake(WITH_BRAKE_ARMS)
-            .withCurrent(MAX_CURRENT)
-            .withMeterMotor(GEAR_RATIO_ARMS, DIAMETER_ARMS)
-            .withInvert(WITH_INVERT_ARMS);
+        public static final TalonSRXConfig ARMS_MOTOR_CONFIG = new TalonSRXConfig(MOTOR_ID_ARMS, "arms motor")
+                        .withBrake(WITH_BRAKE_ARMS)
+                        .withCurrent(MAX_CURRENT)
+                        .withMeterMotor(GEAR_RATIO_ARMS, DIAMETER_ARMS)
+                        .withInvert(WITH_INVERT_ARMS);
 
-    public static final TalonFXConfig LEVER_MOTOR_CONFIG = new TalonFXConfig(MOTOR_ID_LEVER, CANBUS, "motor lever")
-            .withBrake(WITH_BRAKE_LEVER)
-            .withCurrent(MAX_CURRENT)
-            .withInvert(WITH_INVERT_LEVER)
-            .withRadiansMotor(LEVER_GEAR_RATIO);
+        public static final TalonFXConfig LEVER_MOTOR_CONFIG = new TalonFXConfig(MOTOR_ID_LEVER, CANBUS, "motor lever")
+                        .withBrake(WITH_BRAKE_LEVER)
+                        .withCurrent(MAX_CURRENT)
+                        .withInvert(WITH_INVERT_LEVER)
+                        .withRadiansMotor(LEVER_GEAR_RATIO);
 
-    public static final DigitalEncoderConfig DIGITAL_ENCODER_CONFIG = new DigitalEncoderConfig(9, "climb encoder")
-            .withInvert(false)
-            .withOffset(armsOffset);
+        public static final DigitalEncoderConfig DIGITAL_ENCODER_CONFIG = new DigitalEncoderConfig(9, "climb encoder")
+                        .withInvert(false)
+                        .withOffset(armsOffset);
 
-    public enum CLIMB_STATE {
-        IDLE,
-        CLOSE,
-        PREP_CLIMB,
-        CLIMB,
-        GET_OFF_CLIMB,
-        TESTING;
-    }
+        public enum CLIMB_STATE {
+                IDLE,
+                CLOSE,
+                PREP_CLIMB,
+                CLIMB,
+                GET_OFF_CLIMB,
+                TESTING;
+        }
 }
