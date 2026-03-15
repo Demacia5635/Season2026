2a2312ea26b80c3b54901f0134e8ef0f48432dd4
Matan Dizengof <diz.matan@gmail.com>
2026-03-15 22:49:47 +0200

    added back turret

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -105,7 +105,7 @@ public class RobotContainer implements Sendable {
     ledManager = new LedManager();
     leds = new RobotBLedStrip();
     chassis = new Chassis(RobotBChassisConstants.CHASSIS_CONFIG);
-    // turret = Turret.getInstance();
+    turret = Turret.getInstance();
     // ballCamera = new Dvirs_ObjectPose(
     // new Camera("balls", new Translation3d(0.27, -0.15, 0.308), -30, 22, false,
     // true));
